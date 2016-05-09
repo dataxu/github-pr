16e69d33eba1f1e15019e28166f1fe43f568723a
Matt Babineau <mbabineau@dataxu.com>
2016-05-09 13:55:23 -0400

    playing around with PyGithub, squash

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     },
     install_requires=[
         'argparse==1.3.0',
-        'PyGithub==1.25.2',
+        'PyGithub==1.25.2-squash',
         'wsgiref==0.1.2',
         'tabulate==0.7.5'
     ],

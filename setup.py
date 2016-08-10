6998d2effb43c9854d81adacd10a2489281b80da
Matt Babineau <babineaum@users.noreply.github.com>
2016-08-10 16:10:29 -0400

    Bump version to release to pypi (#22)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         'tabulate'
     ],
     url='https://github.com/dataxu/github-pr',
-    version='1.1.1',
+    version='1.5.0',
     scripts=['github_pr.py'],
     keywords=['github'],
 )

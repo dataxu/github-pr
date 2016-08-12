af7cb2796ba2ed6b6c44e5d09870add9869f9802
Matt Babineau <babineaum@users.noreply.github.com>
2016-08-12 11:36:23 -0400

    Bump version for deploy to PyPi (#25)
    I released to PyPi the version 2.0.0 off a branch. I changed the way we install PyGithub after that upload(deploy) to PyPi, so now that we added that change and merged it to master we need a new version. 
    
    TL;DR - bump version because we now install PyGithub from PyPi not Github.com

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'tabulate'
     ],
     url='https://github.com/dataxu/github-pr',
-    version='2.0.0',
+    version='2.0.1',
     scripts=['github_pr.py'],
     keywords=['github']
 )

1ab42d18cf1717d1233fd80ccb13587961b8db6e
Matt Babineau <mbabineau@dataxu.com>
2018-04-18 14:52:16 -0400

    bump version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'tabulate'
     ],
     url='https://github.com/dataxu/github-pr',
-    version='2.3.2',
+    version='2.3.4',
     scripts=['github_pr.py'],
     keywords=['github']
 )
